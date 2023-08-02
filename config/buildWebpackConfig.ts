5f32fb530f6d394108f25fba23ba2e0044fdcb84
OLEG GANIN <88538863+Aleg3000@users.noreply.github.com>
2023-08-02 17:57:47 +0300

    initial commit

---
+++ config/buildWebpackConfig.ts
@@ -0,0 +1,35 @@
+import { Configuration } from "webpack";
+import { buildDevServer } from "./buildDevServer";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import { BuildOptions } from "./types/config";
+
+export const buildWebpackConfig = (options: BuildOptions): Configuration => {
+
+    const { mode, paths, isDev } = options
+
+    return {
+        mode: 'production',
+        entry: paths.entry,
+        output: {
+          filename: '[name].[contenthash].js',
+          path: paths.build,
+          clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+          rules: buildLoaders(options)
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+        
+        //
+        performance: {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+        },
+      };
+}
