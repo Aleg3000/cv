556fe874d5f94b360361164784b2af49f7c3768d
OLEG GANIN <88538863+Aleg3000@users.noreply.github.com>
2023-08-31 14:40:54 +0300

    add project data

---
+++ src/entities/Works/data/data.ts
@@ -0,0 +1,27 @@
+interface Project {
+    title: string
+    description: string
+}
+
+export const projectData: Project[] = [
+    {
+        title: 'Project-D',
+        description: 'this is project 1 ldldlsfsdlfwlsdfl'
+    },
+    {
+        title: 'Worms',
+        description: 'this is project 2 sdfacvaSDfvdrsvl'
+    },
+    {
+        title: 'Boardello',
+        description: 'this is project 3 qDWFEWRFGERGFSDV'
+    },
+    {
+        title: 'Diver Port',
+        description: 'this is project 4 OLJMLKNMJKLDFNGERWJKNl'
+    },
+    {
+        title: 'Final',
+        description: 'this is project 5 ewdwfeskdrfmjekrlg'
+    }
+]
