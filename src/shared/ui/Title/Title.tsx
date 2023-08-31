d99b3e5756f07a7aa41a3badf9cc1d0838ff8935
OLEG GANIN <88538863+Aleg3000@users.noreply.github.com>
2023-08-31 14:40:45 +0300

    add title component

---
+++ src/shared/ui/Title/Title.tsx
@@ -0,0 +1,32 @@
+import { forwardRef } from 'react'
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './Title.module.scss'
+
+interface TitleProps {
+    className?: string
+    text: string
+    onClick?: () => void
+}
+
+const Title = forwardRef<HTMLHeadingElement, TitleProps>((props, ref) => {
+    const { className, text, onClick } = props
+
+    // const titleRef = ref as MutableRefObject<HTMLHeadingElement>
+
+    // useEffect(() => {
+    //     gsap.to(titleRef.current, {
+    //         opacity: 1,
+    //         duration: 5
+    //     })
+    // }, [titleRef])
+
+    return (
+        <h1 ref={ref} onClick={onClick} className={classNames(cls.Title, [className])}>
+            {text.split('').map((letter, index) => {
+                return <span className='titleSpan' key={letter + String(index)}>{letter}</span>
+            })}
+        </h1>
+    )
+})
+
+export default Title
