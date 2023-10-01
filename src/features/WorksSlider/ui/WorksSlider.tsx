e871585261f174809cc17f8cdf2acd67b1c2c31c
OLEG GANIN <88538863+Aleg3000@users.noreply.github.com>
2023-10-01 20:27:16 +0300

    add WorksSlider component

---
+++ src/features/WorksSlider/ui/WorksSlider.tsx
@@ -0,0 +1,19 @@
+import { typedMemo } from 'app/types/memo'
+import { projectData } from 'entities/Works/data/data'
+import { forwardRef, useRef } from 'react'
+import Card from 'shared/ui/Card/Card'
+import Slider from 'shared/ui/Slider/Slider'
+import cls from './WorksSlider.module.scss'
+
+export const WorksSlider = typedMemo(forwardRef<HTMLDivElement>((_, ref) => {
+    const wrapper = useRef<HTMLDivElement>(null)
+    return (
+        <div ref={ref} className={cls.WorksSliderWrapper}>
+            <div className={cls.worksSliderVisibleContainer}>
+                <Slider main={wrapper} className={cls.WorksSlider}>
+                    {projectData.map((project, index) => <Card key={index} image={project.imageM} title={project.title} description={project.description} />)}
+                </Slider>
+            </div>
+        </div>
+    )
+}))
