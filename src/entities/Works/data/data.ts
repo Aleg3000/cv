f652fb55929e1c3412c8b2e3ea063d93e5a0fc30
OLEG GANIN <88538863+Aleg3000@users.noreply.github.com>
2023-10-04 21:15:33 +0300

    add project links component

--- src/entities/Works/data/data.ts
+++ src/entities/Works/data/data.ts
@@ -14,6 +14,8 @@ interface Project {
     stack: string[]
     imageD: string
     imageM: string
+    github: string
+    worldWide: string
 }
 
 export const projectData: Project[] = [
@@ -22,27 +24,35 @@ export const projectData: Project[] = [
         description: 'Web studio\'s site, developed using React and GSAP, is a minimalist showcase of our work. It features smooth animations, highlighting our commitment to modern web design.',
         stack: ['react', 'scss'],
         imageD: projectDImageD,
-        imageM: projectDImageM
+        imageM: projectDImageM,
+        github: 'https://github.com/Aleg3000/Project-d',
+        worldWide: 'http://www.project-done.com/'
     },
     {
         title: 'Worms',
         description: 'Our final study project is an exciting Worms game clone, meticulously crafted by our team of three. We implemented everything from scratch, including a bespoke physics engine, using TypeScript and Three.js to deliver a thrilling gaming experience.',
         stack: ['three.js', 'typescript', 'scss'],
         imageD: wormsImageD,
-        imageM: wormsImageM
+        imageM: wormsImageM,
+        github: 'https://github.com/Aleg3000/RSWorms',
+        worldWide: 'https://wsko-12.github.io/RSWorms/client/'
     },
     {
         title: 'Boardello',
         description: 'Our final project is a cutting-edge task manager that we built using React and TypeScript. This robust tool not only simplifies task organization but also showcases our expertise in modern web development.',
         stack: ['react', 'redux', 'scss'],
         imageD: boardelloDImageD,
-        imageM: boardelloImageM
+        imageM: boardelloImageM,
+        github: 'https://github.com/Aleg3000/project-management-app',
+        worldWide: 'https://marat1000.github.io/project-management-app/'
     },
     {
         title: 'Diver Port',
         description: 'Our portfolio website, brought to life with the magic of WebGL, serves as a captivating canvas for a talented creative designer. Explore their work in a visually stunning and interactive way.',
         stack: ['vanilla js', 'ogl', 'prismic', 'shaders'],
         imageD: diverImageD,
-        imageM: diverImageM
+        imageM: diverImageM,
+        github: 'https://github.com/Aleg3000/nastya-diver-portfolio',
+        worldWide: 'http://185.164.172.21:3000/'
     }
 ]
