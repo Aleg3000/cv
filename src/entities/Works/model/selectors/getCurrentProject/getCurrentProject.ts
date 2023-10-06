b6ed7774b204e283256d02a99fc6400f376217db
OLEG GANIN <88538863+Aleg3000@users.noreply.github.com>
2023-10-06 21:21:24 +0300

    update works selectors

--- src/entities/Works/model/selectors/getCurrentProject/getCurrentProject.ts
+++ src/entities/Works/model/selectors/getCurrentProject/getCurrentProject.ts
@@ -2,3 +2,4 @@ import { type StateSchema } from 'app/providers/StoreProvider'
 
 export const getCurrentProject = (state: StateSchema): number => state.works.currentProject
 export const getIsProjectChanging = (state: StateSchema): boolean => state.works.isProjectChanging
+export const getProjectQuantity = (state: StateSchema): number => state.works.projectsQuantity
